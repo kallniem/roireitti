1353caea60437631cab9b75907b1e213f291353c
Kalle <kallniem@lapinamk.fi>
2026-04-07 08:57:11 +0300

    Add icons for PWA

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -16,6 +16,7 @@ function HomePage() {
                 <h1>Rovaniemen pyöräreitit<br></br>yhdessä paikassa</h1>
                 <p>RoiReitti on Rovaniemen pyöräreittien tietopankki, joka tarjoaa kattavat tiedot reiteistä, niiden vaativuudesta ja pituudesta. Tutustu reitteihin, suunnittele oma pyöräretkesi ja nauti Rovaniemen upeista maisemista!</p>
                 <p>Suuntaa 'Reitit' tai 'Kartta' -näkymään selataksesi reittejä.</p>
+                <InstallPrompt />
             </div>
         </div>
     )
